08ebb733cbf51bdf4886f9ef3349c81868647231
LSJ <1225419368@qq.com>
2023-04-19 10:21:16 +0800

    第一次更新 v1.2
    修改了一些Config文件

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -18,7 +18,7 @@ public class Config {
     public static final String URL = "jdbc:mysql://localhost:3306/" + DATABASE_NAME +
             "?serverTimezone=Asia/Shanghai&characterEncoding=utf-8&useSSL=false";
 
-    public static final String WEB_URL_BEGIN = "http://localhost:8080/Group4Project/";
+    public static final String WEB_URL_BEGIN = "http://localhost:8080/Group1Project/";
     public static final int MEMORY_THRESHOLD = 1024 * 1024 * 3;
     public static final int MAX_FILE_SIZE = 1024 * 1024 * 50;
     public static final int MAX_REQUEST_SIZE = 1024 * 1024 * 60;
